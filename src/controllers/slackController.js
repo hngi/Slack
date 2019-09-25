259a6e096a0007f749bfa2217871605371835878
Js_genin <igunnuemmanuel@gmail.com>
2019-09-26 00:08:30 +0100

    test

--- src/controllers/slackController.js
+++ src/controllers/slackController.js
@@ -35,10 +35,10 @@ export const initButtonConfirmation = (req, res, next) => {
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = '../config/token.json';
+const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
-fs.readFile('../config/credentials.json', (err, content) => {
+fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
     // Authorize a client with credentials, then call the Google Drive API.
     //authorize(JSON.parse(content), listFiles);
