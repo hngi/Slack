add41863c9fa65abb93c52f71af242d0ac8e7297
shonubijerry <shonubijerry@gmail.com>
2019-09-25 11:43:01 +0100

    refactor code

--- src/controllers/slackController.js
+++ src/controllers/slackController.js
@@ -6,7 +6,7 @@ export const getSlashCommandInfo = (req, res) => {
   res.status(200).end(); // best practice to respond with 200 status
   const payload = req.body;
   const responseURL = payload.response_url;
-  const postOptions = prepareRequestMessage(responseURL, initMessage);
+  const postOptions = prepareRequestMessage('POST', responseURL, initMessage);
   request(postOptions, (error, response, body) => {
     if (error) {
       console.log(error);
@@ -23,28 +23,20 @@ export const initButtonConfirmation = (req, res) => {
   console.log(responsePayload);
   if (responsePayload.actions[0].name === 'no') {
     message = {
-      response_type: 'ephemeral',
       text: `Bye ${responsePayload.user.name}`,
     };
-    res.send(message);
-    return;
+  } else {
+    message = {
+      text: `${responsePayload.user.name} your conversation will be saved to Google Drive`,
+    };
   }
-  message = {
-    response_type: 'ephemeral',
-    text: `${responsePayload.user.name} your conversation will be saved to Google Drive`,
-  };
-  res.send(message);
-  // get slack channel history
-  // google drive auth function is called here
-  return;
-
-  // const postOptions = prepareRequestMessage(responsePayload.response_url, message);
-  // request(postOptions, (error, response, body) => {
-  //   if (error) {
-  //     console.log(error);
-  //   }
-  //   return;
-  //   // get slack channel history
-  //   // google drive auth function is called here
-  // });
+  const postOptions = prepareRequestMessage('POST', responsePayload.response_url, message);
+  request(postOptions, (error, response, body) => {
+    if (error) {
+      console.log(error);
+    }
+    return;
+    // get slack channel history
+    // google drive auth function is called here
+  });
 };
